1896b3f5b123f27df048b254e92b99b80b863b33
manerger-manu <144687423+manerger-manu@users.noreply.github.com>
2023-10-04 14:18:57 +0200

    Update jquery.js

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,1 +1,37 @@
-
+$(function(){
+    $('#events').hide();/*oculta envents*/
+    $('#contacto').hide();/*oculta envents*/
+    $('#form').hide();/*oculta envents*/
+    $('.index').on('click',function(event){
+        event.preventDefault();
+        /*$(this).parent().parent().parent().next().toggle();oculta index */
+        $('#index').show();/*oculta envents*/
+        $('#events').hide();/*oculta envents*/
+        $('#contacto').hide();/*oculta envents*/
+        $('#form').hide();/*oculta envents*/
+    });
+    $('.events').on('click',function(event){
+        event.preventDefault();
+        /*$(this).parent().parent().parent().next().toggle();oculta index */
+        $('#index').hide();/*oculta envents*/
+        $('#events').show();/*oculta envents*/
+        $('#contacto').hide();/*oculta envents*/
+        $('#form').hide();/*oculta envents*/
+    });
+    $('.contacto').on('click',function(event){
+        event.preventDefault();
+        /*$(this).parent().parent().parent().next().toggle();oculta index */
+        $('#index').hide();/*oculta envents*/
+        $('#events').hide();/*oculta envents*/
+        $('#contacto').show();/*oculta envents*/
+        $('#form').hide();/*oculta envents*/
+    });
+    $('.form').on('click',function(event){
+        event.preventDefault();
+        /*$(this).parent().parent().parent().next().toggle();oculta index */
+        $('#index').hide();/*oculta envents*/
+        $('#events').hide();/*oculta envents*/
+        $('#contacto').hide();/*oculta envents*/
+        $('#form').show();/*oculta envents*/
+    });
+})
